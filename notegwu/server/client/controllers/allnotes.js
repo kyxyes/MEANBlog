6f076e5bf9236a3778e6af423a1fa8fce8c28147
kyxyes <kyxyes@gmail.com>
2015-05-02 00:51:42 -0400

    del

--- notegwu/server/client/controllers/allnotes.js
+++
@@ -1,6 +0,0 @@
-angular.module('NoteGW')
-       .controller('allnotesCtrl',function($scope,$http){
-        $http.get('/allnotes').success(function(data){
-            $scope.notes = data;
-        });
-    });
