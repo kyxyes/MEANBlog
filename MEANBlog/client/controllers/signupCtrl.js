490da59f98dc2d51349e18e629572be77f695f5f
Yuxin Kang <kyxyes@gmail.com>
2015-05-06 11:59:57 -0400

    Update signupCtrl.js

--- MEANBlog/client/controllers/signupCtrl.js
+++ MEANBlog/client/controllers/signupCtrl.js
@@ -4,11 +4,7 @@ angular.module('MEANBlog')
      $scope.signup = function(){
          var newuser = {username:$scope.username,
                         password:$scope.password,
-<<<<<<< HEAD
                         email:$scope.email}       //ng-model
-=======
-                        email:$scope.email}
->>>>>>> ee198e5a59e03dc3ab9f671c055bd528423e8975
          $http.post('/signup',newuser);
      }
     }]);
